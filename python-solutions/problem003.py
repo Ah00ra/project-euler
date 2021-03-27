fd0cfb8ec114000b88f6b7ad161bf615db60d21f
imahoora <naghipour.ahoora@gmail.com>
2021-03-27 13:59:43 +0430

    Add problem003

---
+++ python-solutions/problem003.py
@@ -0,0 +1,19 @@
+def smallest_prime_factor(n):
+    for i in range(2, int(n**0.5)+1):
+    	if n % i == 0:
+    		return i
+
+    return n
+
+
+def find_largest_prime_factor(number):
+    while True:
+        smallest_factor = smallest_prime_factor(number)
+
+        if smallest_factor < number:
+            number //= smallest_factor
+        else:
+            return number
+
+
+print(find_largest_prime_factor(600851475143))
