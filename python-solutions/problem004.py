a3f2a0133f647291adab4fba159db40d1b36bc9b
imahoora <naghipour.ahoora@gmail.com>
2021-03-28 19:01:42 +0430

    Update problem004 delete extra spaces (my editor mistake)

--- python-solutions/problem004.py
+++ python-solutions/problem004.py
@@ -1,25 +1,25 @@
 def is_palindromic(n):
-	n = str(n)
-	if n == n[::-1]:
-		return True
-	return False
+    n = str(n)
+    if n == n[::-1]:
+        return True
+    return False
 
 
 def main():
-	lst = [i for i in range(100, 1000)]
-	counter = 0
-	answer = 0
-	ans = 0
-	while lst[counter] < 999:
-		for index in range(len(lst)):
-			ans = lst[counter] * lst[index]
+    lst = [i for i in range(100, 1000)]
+    counter = 0
+    answer = 0
+    ans = 0
+    while lst[counter] < 999:
+        for index in range(len(lst)):
+            ans = lst[counter] * lst[index]
 
-			if is_palindromic(ans):
-				if ans > answer:
-					answer = ans
-		counter += 1
-	return answer
+            if is_palindromic(ans):
+                if ans > answer:
+                    answer = ans
+        counter += 1
+    return answer
 
 
 if __name__ == "__main__":
-	print(main())
+    print(main())
