272c6a75784defc80aed43c247685ae64f916409
imahoora <naghipour.ahoora@gmail.com>
2021-03-29 11:54:38 +0430

    Update problem002 delete extra spaces (my editor mistake)

--- python-solutions/problem002.py
+++ python-solutions/problem002.py
@@ -1,20 +1,20 @@
 def is_even(n):
-	if n % 2 == 0:
-		return True
-	return False
+    if n % 2 == 0:
+            return True
+    return False
 
 
 def main():
-	n1, n2 = 1, 2
-	nth = n1 + n2
-	summ = 0
-	while n1 < 4000000:
-		if is_even(n1):
-			summ += n1
-		n1, n2 = n2, nth
-		nth = n1 + n2
-	return summ
+    n1, n2 = 1, 2
+    nth = n1 + n2
+    summ = 0
+    while n1 < 4000000:
+            if is_even(n1):
+                summ += n1
+            n1, n2 = n2, nth
+            nth = n1 + n2
+    return summ
 
 
 if __name__ == "__main__":
-	print(main())
+    print(main())
